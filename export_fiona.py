67ba53f00da7f85bc5a36b1b5320715c21966215
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2014-01-31 10:38:55 +0100

    More

--- export_fiona.py
+++ export_fiona.py
@@ -1,5 +1,8 @@
 #! /bin/python
 
+import logging
+import sys
+
 from sqlalchemy import *
 from sqlalchemy.orm import *
 
@@ -21,14 +24,31 @@ class Ecole(Base):
     __table_args__ = {'autoload': True}
     the_geom = GeometryColumn(Point(2))
 
+GeometryDDL(Ecole.__table__)
 
-s = session.query(Ecole).get(1)
-print session.scalar(s.the_geom.wkt)
+#s = session.query(Ecole).get(1)
+#print s.the_geom.wkt
 
-from shapely.geometry import shape
+s = session.query(Ecole).filter(Ecole.the_geom!=None).limit(10).all()
+#print session.scalar(s.the_geom.wkt)
+
+from shapely.wkb import loads
+from shapely.geometry import mapping
+import fiona
 from fiona import collection
 
-#with fiona.open('/tmp/test_fiona.shp', 'w') as sink:
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
-#for f in s:
-    #geom = shapely.wkb.loads(str(f.the_geom.geom_wkb))
+with fiona.open(
+    '/tmp/test_fiona.tab',
+    'w',
+    driver='MapInfo File',
+    schema={'geometry': 'Point', 'properties': [('numero', 'int')]}) as sink:
+    for row in s:
+        f = {}
+        geom = loads(str(row.the_geom.geom_wkb))
+        f['geometry'] = mapping(geom)
+        f['properties'] = {}
+        f['properties']['numero'] = row.numero
+        sink.write(f)
+        logging.info("Write done")
